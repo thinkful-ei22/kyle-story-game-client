771d95317237281e6501ce373f0752056cb92f95
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-08-10 17:59:56 -0600

    add redux devtools integration for chrome

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import { Route } from 'react-router';
-import { ConnectedRouter } from 'react-router-redux';
-import { routerMiddleware } from 'react-router-redux';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 
 import createHistory from 'history/createBrowserHistory';
 
@@ -31,7 +30,11 @@ let socketIoMiddleware =
 const history = createHistory();
 const routingMiddleware = routerMiddleware(history);
 
-const store = createStore(rootReducer, applyMiddleware(routingMiddleware, socketIoMiddleware, thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(routingMiddleware, socketIoMiddleware, thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
