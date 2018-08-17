13bf57bb5a5217fd1c700b3411a7d25ed3ff287f
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-08-16 23:33:36 -0600

    update player ready indicator

--- src/components/Game/LoadingScreen/PlayersLoadingList/PlayerLoadingStatus/PlayerReadyIndicator/index.jsx
+++ src/components/Game/LoadingScreen/PlayersLoadingList/PlayerLoadingStatus/PlayerReadyIndicator/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import './playerReadyIndicator.css';
+import image from '../../../../../../images/_ionicons_svg_md-checkmark.svg';
+
 export default function PlayerReadyIndicator(props) {
   return (
-    <span>{ props.readyState ? 'Ready' : '...' }</span>
+    <span>{ props.readyState ? 'Ready' : <img src={image} alt='ready' className='ready' /> }</span>
   );
 }
